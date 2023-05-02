e3d3fcd7c54bcfad23759be867475c8ae0b5c1be
Lorelyz <104861703+lorelyz@users.noreply.github.com>
2023-05-01 18:46:15 -0700

    update

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -1,17 +1,24 @@
+
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
 
 // Part 1: Declare (5) Variables With Type
-
+// let spacecraftName: string = 'Determination';
+// let speedMph: number = 17500;
+let kilomitersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+// let milesPerKilometer: number = 0.621
 
 
 // Part 2: Print Days to Mars
-
-
+// let milesToMars: number = kilometersToMars * milesPerKilometer;
+// let hoursToMars: number = milesToMars / speedMph;
+// let daysToMars: number = hoursToMars / 24;
+//console.log('${spacecraftName} would take ${daysToMars} days to get to Mars.');
 
 // Code an output statement here (use a template literal):
-
+// console.log('${spacecraftName}) would take ${daysToMars.toFixed(2)} days to get to Mars.')
 
 
 // Part 3: Create a Function ("getDaysToLocation")
---
+++ studio/Astronaut.ts
@@ -0,0 +1,12 @@
+import { Payload } from './Payload';
+
+export class Astronaut implements Payload {
+    massKg: number;
+    name: string;
+
+
+    constructor(massKg, name) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+}
